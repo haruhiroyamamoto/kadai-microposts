150aeb520bac2407b77480d9b0dbb436e90288e9
haruhiro.yamamoto <haruhiro17yamamoto20@gmail.com>
2020-08-13 14:28:52 +0000

    timetable

--- app/User.php
+++ app/User.php
@@ -40,10 +40,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Micropost::class);
     }
-     public function loadRelationshipCounts()
+     /**public function loadRelationshipCounts()
     {
         $this->loadCount('microposts');
-    }
+    }*/
      /**
      * このユーザがフォロー中のユーザ。（ Userモデルとの関係を定義）
      */
@@ -115,6 +115,15 @@ class User extends Authenticatable
     {
         // フォロー中ユーザの中に $userIdのものが存在するか
         return $this->followings()->where('follow_id', $userId)->exists();
+    }
+     public function feed_microposts()
+    {
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
     }
      /**
      * このユーザに関係するモデルの件数をロードする。
