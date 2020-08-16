24bb9a759d3b2340e68bebf8f0d0cf888134e0fa
haruhiro.yamamoto <haruhiro17yamamoto20@gmail.com>
2020-08-16 02:13:28 +0000

    createmicroposts

--- database/migrations/2020_08_07_083915_create_microposts_table.php
+++ database/migrations/2020_08_07_083915_create_microposts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserMicropostsTable extends Migration
+class CreateMicropostsTable extends Migration
 {
  public function up()
     {
